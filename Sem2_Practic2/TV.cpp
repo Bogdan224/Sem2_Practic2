8391536143e59a6b8d92f0efb918190edbe6696b
Богдан Колганов <kolganov_25@mail.ru>
2024-02-24 20:08:03 +0700

    -Finish Task

--- Sem2_Practic2/TV.cpp
+++ Sem2_Practic2/TV.cpp
@@ -1,6 +1,8 @@
 #include "TV.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 int CheckValues(TV* tv) {
 	if (tv == NULL) {
@@ -26,6 +28,20 @@ int Init(TV* res, char* model, int displaySize, bool connectToWifi) {
 	}
 }
 
+int Init(TV* res, const char* model, int displaySize, bool connectToWifi) {
+	char* m = (char*)malloc(sizeof(char));
+	strcpy_s(m, sizeof(model), model);
+	res->_model = m;
+	res->_displaySize = displaySize;
+	res->_connectToWiFi = connectToWifi;
+	if (CheckValues(res) == 0) {
+		return 0;
+	}
+	else {
+		return -1;
+	}
+}
+
 void Init(TV* tv, TV* original) {
 	tv->_model = original->_model;
 	tv->_displaySize = original->_displaySize;
@@ -40,4 +56,26 @@ void Print(TV* tv) {
 	else {
 		printf("Connect to WiFi: No\n");
 	}
+}
+
+int SetDisplaySize(int displaySize, TV* tv) {
+	int size = tv->_displaySize;
+	tv->_displaySize = displaySize;
+	if (CheckValues(tv)==0) {
+		return 0;
+	}
+	else {
+		tv->_displaySize = size;
+		return -1;
+	}
+}
+
+TV* NewTV() {
+	TV* tv = (TV*)malloc(sizeof(TV));
+	Init(tv, "Samsung", 40, false);
+	return tv;
+}
+
+void DeleteTV(TV* tv) {
+	free(tv);
 }
